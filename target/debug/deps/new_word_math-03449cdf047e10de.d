3180db8ea3f1df3db58589730f533a6516ff31a9
Matthew Rembisz <m.rembisz1@gmail.com>
2023-10-03 01:14:15 -0400

    Uploading rust word calculator

---
+++ target/debug/deps/new_word_math-03449cdf047e10de.d
@@ -0,0 +1,5 @@
+/home/matt/Desktop/Programming/Word-Math/target/debug/deps/new_word_math-03449cdf047e10de: src/main.rs
+
+/home/matt/Desktop/Programming/Word-Math/target/debug/deps/new_word_math-03449cdf047e10de.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/new_word_math.d
@@ -0,0 +1,1 @@
+/home/matt/Desktop/Programming/Word-Math/target/debug/new_word_math: /home/matt/Desktop/Programming/Word-Math/src/main.rs
