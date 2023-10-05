7b4a57a9d126900d5abbc433f137250dc068a63a
Matthew Rembisz <m.rembisz1@gmail.com>
2023-10-05 05:28:38 -0400

    Remove target folder

--- target/debug/deps/new_word_math-03449cdf047e10de.d
+++
@@ -1,5 +0,0 @@
-/home/matt/Desktop/Programming/Rust/word_math/target/debug/deps/new_word_math-03449cdf047e10de: src/main.rs
-
-/home/matt/Desktop/Programming/Rust/word_math/target/debug/deps/new_word_math-03449cdf047e10de.d: src/main.rs
-
-src/main.rs:
